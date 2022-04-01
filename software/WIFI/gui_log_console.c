e384b1e86f2fb71d1fe86bc2acd4fd70d14139f9
HAH <59907145+houaohui@users.noreply.github.com>
2022-04-01 22:51:19 +0800

    Add files via upload

--- software/WIFI/gui_log_console.c
+++ software/WIFI/gui_log_console.c
@@ -4,10 +4,16 @@
 #include "common.h"
 #include "lcd.h"
 
+//支持换行
+#define SUPORT_HUANGHANG    1
+
+//更新行动画
+#define SUPORT_NEW_LINE_AMI 1
 
 #define LINE_BUF_MAX  50
 #define LINE_SHOW_CNT 8 
 #define LINE_CHAR_CNT 17
+#define CHAR_HEIGHT   8
 
 #define LINE(n)              ((n-1)*8)
 
@@ -16,12 +22,12 @@ char firststr_buff[1024];
 
 char now_line=1;
 char new_line_flag=0;
+char LOOP_SHOW_MD=0;
+char NEW_LINE_AMI=0;
 
 static char index=0;
 static unsigned char last_str_length=0;
 char now_str_length;
-
-unsigned char show_last_line_ldx=0;  //偏移行数，控制该变量可以看历史行,
 
 
 void gui_print_s(int x, int y,char *str)
@@ -29,31 +35,11 @@ void gui_print_s(int x, int y,char *str)
 	draw_string(str, false, x, y);
 }
 
-//以前的按键回调函数
-//bool up_line(void)
-//{
-//	show_last_line_ldx++;
-//	if(show_last_line_ldx>=LINE_BUF_MAX-LINE_SHOW_CNT)  //最大偏移行是20-8，再偏移第一行就显示到最后一行了
-//	{
-//		show_last_line_ldx = LINE_BUF_MAX-LINE_SHOW_CNT;
-//	}
-//	return true;
-//}
-//以前的按键回调函数
-//bool down_line(void)
-//{
-//	show_last_line_ldx--;
-//	if(show_last_line_ldx==255)
-//	{
-//		show_last_line_ldx=0;
-//	}
-//	return true;
-//}
+
 
 //显示像素级偏移64-160
-short pix_show_idx=64;
+short pix_show_idx=CHAR_HEIGHT*LINE_SHOW_CNT;
 
-#define CHAR_HEIGHT 8
 
 
 //像素级的按键回调函数
@@ -79,15 +65,36 @@ bool down_line(void)
 
 
 //此函数替换了以前的滚动，现在是像素级滚动，如果想要以前的方式，注释这个，用下面的以前的
-void console_loop_show(void)
+char console_loop_show(void)
 {
+	static char deal_ok=true;
 	
 	char show_index=(LINE_BUF_MAX+index-LINE_SHOW_CNT)%LINE_BUF_MAX;  //第一行索引值
+	
+#if SUPORT_NEW_LINE_AMI
+	
+	
+	if(NEW_LINE_AMI==true&&pix_show_idx!=CHAR_HEIGHT*LINE_SHOW_CNT)
+	{
+		deal_ok=false;
+		pix_show_idx--;
+		if(pix_show_idx<=CHAR_HEIGHT*LINE_SHOW_CNT)
+		{
+			deal_ok=true;
+			pix_show_idx=CHAR_HEIGHT*LINE_SHOW_CNT;
+			NEW_LINE_AMI=false;
+		}
+		
+	}
+	
+	
+#endif
 	
 	//得到相对于show_index向上的偏移行数
 	short pix_offset_line=(pix_show_idx-CHAR_HEIGHT*LINE_SHOW_CNT)/CHAR_HEIGHT+1;   
 	
 	show_index=(LINE_BUF_MAX+show_index-pix_offset_line)%LINE_BUF_MAX;
+	
 	
 	
 	for(char c=0;c<=LINE_SHOW_CNT;c++)  
@@ -96,22 +103,10 @@ void console_loop_show(void)
 		show_index++;
 		if(show_index>=LINE_BUF_MAX)show_index=0;
 	}
+	return deal_ok;
 }
 
 
-//void console_loop_show(void)
-//{
-//	char show_index=(LINE_BUF_MAX+index-LINE_SHOW_CNT)%LINE_BUF_MAX;  //得到没有偏移的第一行索引值
-//	
-//	show_index=(LINE_BUF_MAX+show_index-show_last_line_ldx)%LINE_BUF_MAX;  //在此索引值上偏移show_last_line_ldx
-//	
-//	for(char c=1;c<=LINE_SHOW_CNT;c++)  //显示所有行
-//	{
-//		gui_print_s(0,LINE(c),string_buff[show_index].str_buff);  //依次从第一行显示到第八行
-//		show_index++;
-//		if(show_index>=LINE_BUF_MAX)show_index=0;
-//	}
-//}
 //copycnt-1个字符
 void strcopy_cnt(char* buf, char* src ,char cnt)
 {
@@ -127,6 +122,8 @@ void strcopy_cnt(char* buf, char* src ,char cnt)
 //解决\n前面的\r
 char r_off_set=0;
 char r_kee=0;
+
+
 
 short find_huanhang_str_idx(char* src,unsigned short cnt)
 {
@@ -158,7 +155,7 @@ short find_huanhang_str_idx(char* src,unsigned short cnt)
 	return -1;
 }
 
-#define SUPORT_HUANGHANG 1
+
 
 void console_log(unsigned short time_delay ,char* fmt,...)
 {
@@ -170,8 +167,6 @@ void console_log(unsigned short time_delay ,char* fmt,...)
 	va_end(ap);	
 	
 	
-	show_last_line_ldx=0;  //清除偏移
-	pix_show_idx=CHAR_HEIGHT*LINE_SHOW_CNT;
 	
 	unsigned short All_str_length=strlen((const char*)firststr_buff);	//计算显示总字符长度
 	
@@ -216,39 +211,7 @@ void console_log(unsigned short time_delay ,char* fmt,...)
 		}
 	}
 	
-	
-//	//如果找到，则新行显示
-//	if(this_line_char_cnt!=-1) 
-//	{
-//		//LINE_CHAR_CNT字符内de\n不在结尾
-//		if(this_line_char_cnt+1!=All_str_length)
-//		{
-//			div_flag=1;
-//			strcopy_cnt(string_buff[index].str_buff,firststr_buff,this_line_char_cnt-r_off_set); 
-//			this_line_char_cnt++; //换行在下行不显示
-//		}
-//		else  //LINE_CHAR_CNT字符内de\n在结尾或者在第一个
-//		{
-//			div_flag=0;
-//			if(this_line_char_cnt==0)
-//				strcopy_cnt(string_buff[index].str_buff," ",1);
-//			else
-//				strcopy_cnt(string_buff[index].str_buff,firststr_buff,this_line_char_cnt-r_off_set); 
-//		}
-//	}
-//	else //没有找到，则最大行显示
-//	{
-//		div_flag=All_str_length/LINE_CHAR_CNT;
-//		
-//		//使得显示LINE_CHAR_CNT个字符时也不换行
-//		if(All_str_length==LINE_CHAR_CNT)
-//			div_flag=0;
-//		this_line_char_cnt = LINE_CHAR_CNT;
-//		strcopy_cnt(string_buff[index].str_buff,firststr_buff,this_line_char_cnt); 
-//	}
-//	
-	
-	
+
 	
 	
 	
@@ -271,6 +234,11 @@ void console_log(unsigned short time_delay ,char* fmt,...)
 	}
 	last_str_length=now_str_length;
 	
+	
+	if(index>=LINE_SHOW_CNT)  //大于要显示的行数，就要显示新行
+	{
+		new_line_flag=true;
+	}
 
 	index++;
 	
@@ -279,33 +247,48 @@ void console_log(unsigned short time_delay ,char* fmt,...)
 		index=0;
 	}
 	
-	if(index>=LINE_SHOW_CNT)  //大于要显示的行数，就要显示新行
-	{
-		new_line_flag=1;
-	}
-	
-	if(new_line_flag==0) //直接在下一行打印新行
+	//show
+	if(new_line_flag==false) //直接在下一行打印新行
 	{
 		gui_print_s(0,LINE(index),string_buff[index-1].str_buff);
+		oled_flush();
+		
+		//非loop show模式使用阻塞延时
+		delay_ms(time_delay);
 	}
 	else
 	{
-		memset(&oledBuffer, 0x00, FRAME_BUFFER_SIZE);
+		//底部更新新行标志位
+		NEW_LINE_AMI=true;
+		//每次需要往下偏移形成动画
 		
+#if SUPORT_NEW_LINE_AMI
 		
-		char show_index=(LINE_BUF_MAX+index-LINE_SHOW_CNT)%LINE_BUF_MAX;  //定位最后一行
-		for(char c=1;c<=LINE_SHOW_CNT;c++)  //显示所有行
+		pix_show_idx+=CHAR_HEIGHT;
+		
+#else
+		pix_show_idx=CHAR_HEIGHT*LINE_SHOW_CNT;
+		
+#endif
+		
+		if(LOOP_SHOW_MD==false)
 		{
-			gui_print_s(0,LINE(c),string_buff[show_index].str_buff);  //依次从第一行显示到第八行
-			show_index++;
-			if(show_index>=LINE_BUF_MAX)show_index=0;
+			
+			char sta;
+			do{
+				memset(&oledBuffer, 0x00, FRAME_BUFFER_SIZE);
+				sta = !console_loop_show();
+				oled_flush();
+			}while(sta);
+			//非loop show模式使用阻塞延时
+			delay_ms(time_delay);
 		}
 		
 	}
 	
-	oled_flush();	
 	
-	delay_ms(time_delay);
+	
+	
 	
 #if SUPORT_HUANGHANG  //支持换行
 	
